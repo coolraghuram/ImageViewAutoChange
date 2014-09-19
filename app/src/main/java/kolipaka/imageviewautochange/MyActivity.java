089ec3804bf1839d2155f9d4e166e2e7cd28d1a1
Raghuram <raghuram.kolipaka@gmail.com>
2014-09-19 22:28:52 +0530

    updated

--- app/src/main/java/kolipaka/imageviewautochange/MyActivity.java
+++ app/src/main/java/kolipaka/imageviewautochange/MyActivity.java
@@ -4,14 +4,19 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 
 public class MyActivity extends Activity {
+    private ImageView imgView;
+    private
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        imgView = (ImageView) findViewById(R.id.)
     }
 
 
